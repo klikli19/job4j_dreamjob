5300012ebab87bb118cd9f823dd3321f93e9d136
Anna Kilikova <103356373+klikli19@users.noreply.github.com>
2025-01-13 21:37:39 +0300

    Список кандидатов

---
+++ src/main/java/ru/job4j/dreamjob/repository/MemoryCandidateRepository.java
@@ -0,0 +1,53 @@
+package ru.job4j.dreamjob.repository;
+
+import ru.job4j.dreamjob.model.Candidate;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+public class MemoryCandidateRepository implements CandidateRepository {
+
+    private static final MemoryCandidateRepository INSTANCE = new MemoryCandidateRepository();
+
+    private int nextId = 1;
+
+    private final Map<Integer, Candidate> candidates = new HashMap<>();
+
+    private MemoryCandidateRepository() {
+        save(new Candidate(0, "Вася Иванов", "Intern", LocalDateTime.now()));
+        save(new Candidate(0, "Алеша Смирнов", "Junior", LocalDateTime.now()));
+        save(new Candidate(0, "Миша Петров", "Junior+", LocalDateTime.now()));
+        save(new Candidate(0, "Семён Титов", "Middle", LocalDateTime.now()));
+        save(new Candidate(0, "Рома Никитин", "Middle+", LocalDateTime.now()));
+        save(new Candidate(0, "Вова Ларин", "Senior", LocalDateTime.now()));
+    }
+    public static MemoryCandidateRepository getInstance() {
+        return INSTANCE;
+    }
+    @Override
+    public Candidate save(Candidate candidate) {
+        candidate.setId(nextId++);
+        candidates.put(candidate.getId(), candidate);
+        return candidate;
+    }
+    @Override
+    public void deleteById(int id) {
+        candidates.remove(id);
+    }
+    @Override
+    public boolean update(Candidate candidate) {
+        return candidates.computeIfPresent(candidate.getId(),
+                (id, oldCandidate) -> new Candidate(oldCandidate.getId(), candidate.getName())) != null;
+    }
+    @Override
+    public Optional<Candidate> findById(int id) {
+        return Optional.ofNullable(candidates.get(id));
+    }
+    @Override
+    public Collection<Candidate> findAll() {
+        return candidates.values();
+    }
+}
